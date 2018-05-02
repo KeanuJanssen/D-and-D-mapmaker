0d88eba284bfe6ea12a902d7e48e22d8798d57f4
Lennart  Bronner <D218908@edu.rocwb.nl>
2018-05-02 12:06:45 +0200

    View maps pagina front-end

--- mapmaker/resources/views/maps/view.blade.php
+++ mapmaker/resources/views/maps/view.blade.php
@@ -2,17 +2,122 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-start">
+    <div class="row justify-content-start d-flex justify-content-between flex-wrap">
         <!-- Map display -->
-        <div class="col-md-2" style="background:olive;">
-            <div class="image-container">
-                <img src="{{ asset('img/placeholder-map-image.jpg') }}" alt="">
+        <div class="col-md-3 map-wrapper">
+            <div class="map-container">
+                <div class="image-container">
+                    <img src="{{ asset('img/placeholder-map-image.jpg') }}" alt="">
+                </div>
+                <div class="map-caption d-flex justify-content-between align-items-center">
+                    <p class="map-title">Map titel</p>
+                    <div class="button-section">
+                        <a class="btn btn-info" href="#"><i class="fas fa-pencil-alt"></i></a>
+                        <a class="btn btn-danger" href="#"><i class="far fa-times-circle"></i></a>
+                    </div>
+                </div>
             </div>
-            <div class="map-caption">
-                <p class="map-title"></p>
-                <a class="" href=""></a>
-                <a class="" href=""></a>
+        </div>
+        <div class="col-md-3 map-wrapper">
+            <div class="map-container">
+                <div class="image-container">
+                    <img src="{{ asset('img/placeholder-map-image.jpg') }}" alt="">
+                </div>
+                <div class="map-caption d-flex justify-content-between align-items-center">
+                    <p class="map-title">Map titel</p>
+                    <div class="button-section">
+                        <a class="btn btn-info" href="#"><i class="fas fa-pencil-alt"></i></a>
+                        <a class="btn btn-danger" href="#"><i class="far fa-times-circle"></i></a>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div class="col-md-3 map-wrapper">
+            <div class="map-container">
+                <div class="image-container">
+                    <img src="{{ asset('img/placeholder-map-image.jpg') }}" alt="">
+                </div>
+                <div class="map-caption d-flex justify-content-between align-items-center">
+                    <p class="map-title">Map titel</p>
+                    <div class="button-section">
+                        <a class="btn btn-info" href="#"><i class="fas fa-pencil-alt"></i></a>
+                        <a class="btn btn-danger" href="#"><i class="far fa-times-circle"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 map-wrapper">
+            <div class="map-container">
+                <div class="image-container">
+                    <img src="{{ asset('img/placeholder-map-image.jpg') }}" alt="">
+                </div>
+                <div class="map-caption d-flex justify-content-between align-items-center">
+                    <p class="map-title">Map titel</p>
+                    <div class="button-section">
+                        <a class="btn btn-info" href="#"><i class="fas fa-pencil-alt"></i></a>
+                        <a class="btn btn-danger" href="#"><i class="far fa-times-circle"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 map-wrapper">
+            <div class="map-container">
+                <div class="image-container">
+                    <img src="{{ asset('img/placeholder-map-image.jpg') }}" alt="">
+                </div>
+                <div class="map-caption d-flex justify-content-between align-items-center">
+                    <p class="map-title">Map titel</p>
+                    <div class="button-section">
+                        <a class="btn btn-info" href="#"><i class="fas fa-pencil-alt"></i></a>
+                        <a class="btn btn-danger" href="#"><i class="far fa-times-circle"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 map-wrapper">
+            <div class="map-container">
+                <div class="image-container">
+                    <img src="{{ asset('img/placeholder-map-image.jpg') }}" alt="">
+                </div>
+                <div class="map-caption d-flex justify-content-between align-items-center">
+                    <p class="map-title">Map titel</p>
+                    <div class="button-section">
+                        <a class="btn btn-info" href="#"><i class="fas fa-pencil-alt"></i></a>
+                        <a class="btn btn-danger" href="#"><i class="far fa-times-circle"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 map-wrapper">
+            <div class="map-container">
+                <div class="image-container">
+                    <img src="{{ asset('img/placeholder-map-image.jpg') }}" alt="">
+                </div>
+                <div class="map-caption d-flex justify-content-between align-items-center">
+                    <p class="map-title">Map titel</p>
+                    <div class="button-section">
+                        <a class="btn btn-info" href="#"><i class="fas fa-pencil-alt"></i></a>
+                        <a class="btn btn-danger" href="#"><i class="far fa-times-circle"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 map-wrapper">
+            <div class="map-container">
+                <div class="image-container">
+                    <img src="{{ asset('img/placeholder-map-image.jpg') }}" alt="">
+                </div>
+                <div class="map-caption d-flex justify-content-between align-items-center">
+                    <p class="map-title">Map titel</p>
+                    <div class="button-section">
+                        <a class="btn btn-info" href="#"><i class="fas fa-pencil-alt"></i></a>
+                        <a class="btn btn-danger" href="#"><i class="far fa-times-circle"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 new-map-button btn btn-primary">
+            <p>Maak hier je eerste map aan!</p>
         </div>
     </div>
 </div>
