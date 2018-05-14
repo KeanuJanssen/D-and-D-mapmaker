08fd51eb0a8ec7c5d9bfcf3e40e56666ab09a5b1
Lennart  Bronner <D218908@edu.rocwb.nl>
2018-05-14 11:13:30 +0200

    Front-end create map pagina

--- mapmaker/resources/views/maps/create.blade.php
+++ mapmaker/resources/views/maps/create.blade.php
@@ -4,18 +4,198 @@
 <div class="container-fluid">
    <div class="row">
        <div class="col vh-87-1" style="background: lightgrey;">
-           <div class="row py-4">
-                <div class="col-12">
-                    nog een hoofdmenu ook
+           <div class="row py-4 no-gutters">
+                <div class="main-sprites col-12 row no-gutters border-bottom border-dark">
+                    <div class="col-2"><img class="w-100" src="{{ asset('img/placeholder-map-image.jpg') }}" alt=""></div>
+                    <div class="col-2"><img class="w-100" src="{{ asset('img/placeholder-map-image.jpg') }}" alt=""></div>
+                    <div class="col-2"><img class="w-100" src="{{ asset('img/placeholder-map-image.jpg') }}" alt=""></div>
+                    <div class="col-2"><img class="w-100" src="{{ asset('img/placeholder-map-image.jpg') }}" alt=""></div>
+                    <div class="col-2"><img class="w-100" src="{{ asset('img/placeholder-map-image.jpg') }}" alt=""></div>
+                    <div class="col-2"><img class="w-100" src="{{ asset('img/placeholder-map-image.jpg') }}" alt=""></div>
+                </div>
+                <div class="sub-sprites col-12 row no-gutters">
+                    <div class="col-2"><img class="w-100" src="{{ asset('img/placeholder-map-image.jpg') }}" alt=""></div>
+                    <div class="col-2"><img class="w-100" src="{{ asset('img/placeholder-map-image.jpg') }}" alt=""></div>
                 </div>
            </div>
        </div>
        <div class="col-10">
            <div class="row" style="height: 75vh!important;">
-               hier komt de mega vette map
+                <div class="col-12 row no-gutters">
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                </div>
+                <div class="col-12 row no-gutters">
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                </div>
+                <div class="col-12 row no-gutters">
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                </div>
+                <div class="col-12 row no-gutters">
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                </div>
+                <div class="col-12 row no-gutters">
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                </div>
+                <div class="col-12 row no-gutters">
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                </div>
+                <div class="col-12 row no-gutters">
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                </div>
+                <div class="col-12 row no-gutters">
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                    <div class="col sprite-slot"></div>
+                </div>
            </div>
            <div class="row" style="height: 12vh!important; background: grey;">
-               hier komt een menutje
+                <div class="col-3 d-flex justify-content-center align-items-center">
+                    <button class="btn btn-primary bottom-menu-buttons">Weergaven</button>
+                </div>
+                <div class="col-3 d-flex justify-content-center align-items-center flex-column">
+                    <div class="badge badge-light bottom-menu-buttons w-100">Papier grote: A4</div>
+                    <div class="badge badge-light bottom-menu-buttons w-100">Sprite grote: 2,5</div>
+                </div>
+                <div class="col-3 d-flex justify-content-center align-items-center">
+                    <button class="btn btn-danger bottom-menu-buttons">Verwijderen</button>
+                </div>
+                <div class="col-3 d-flex justify-content-center align-items-center">
+                    <button class="btn btn-success bottom-menu-buttons">Opslaan</button>
+                </div>
            </div>
        </div>
    </div>
