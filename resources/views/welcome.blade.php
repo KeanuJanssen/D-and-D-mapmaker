2fddfa3044b01d448a5d9820e94923c4434d4dae
Lennart  Bronner <D218908@edu.rocwb.nl>
2018-06-06 11:46:34 +0200

    optimized images second try

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
     <div class="row home-page">
         <div class="col-10 backgrounded d-flex justify-content-center align-items-center">
             <div class="intro-text">
-                <h2>Hallo en dit is een test bericht</h2>
-                <p>Super handig toch zo'n mooi test bericht? Er staat heel veel tekst in en het is mooier dan je zou denken. Normaal gebruik ik hier Lorem ipsum voor maar aangezien de emmet van Visual studio code dat niet ondersteunt doe ik het even op deze manier. Nu ik er zo over nadenk kan ik ook naar de site gaan van Lorem ipsum en daar een tekst van kopiëren en plakken. Bedankt brein!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam euismod tortor in dui cursus viverra. Etiam pretium augue id massa tincidunt facilisis. Quisque ante tellus, dignissim in ultrices eu, placerat bibendum nunc. Morbi id augue non tellus facilisis imperdiet ut ut turpis. Donec consequat mi vel lectus mollis tempor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc in lacinia odio. Nam dapibus vitae nibh ac porttitor.</p>
+                <h2>Welkom bij de Dungeons & Dragons mapmaker!</h2>
+                <p>Heb je ook genoeg van het telkens weer opnieuw tekenen van de dungeons in je campaign? Vraag je je af of er misschien een makkelijkere manier is? Hier is de oplossing! Met D&D mapmaker kun je zelf door middel van drag&drop functionaliteiten een speelveld maken.</p>
+                <p></p>
             </div>
         </div>
         <div class="col-2 login-options">
