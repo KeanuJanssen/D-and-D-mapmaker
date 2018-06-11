953cd74e960ddb0f0d95e02ed9dd3eb20b46d7f7
KeanuJanssen <38426534+KeanuJanssen@users.noreply.github.com>
2018-06-11 13:15:26 +0200

    reïnstall database migrations etc
    migrations,
    seeds,
    appServerProvider bug fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('accountstatus')->default(0);
+            $table->string('zipcode');
+            $table->string('address');
+            $table->string('city');
             $table->rememberToken();
             $table->timestamps();
         });
