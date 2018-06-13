e45733739168fd8a7d937d129b693449a0a86140
Lennart  Bronner <D218908@edu.rocwb.nl>
2018-06-13 16:20:40 +0200

    Welkom tekst aangepast

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
         <div class="col-10 backgrounded d-flex justify-content-center align-items-center">
             <div class="intro-text">
                 <h2>Welkom bij de Dungeons & Dragons mapmaker!</h2>
-                <p>Heb je ook genoeg van het telkens weer opnieuw tekenen van de dungeons in je campaign? Vraag je je af of er misschien een makkelijkere manier is? Hier is de oplossing! Met D&D mapmaker kun je zelf door middel van drag&drop functionaliteiten een speelveld maken.</p>
-                <p></p>
+                <p>Heb je ook genoeg van het telkens weer opnieuw tekenen van de dungeons voor je D&D sessie? Of zoek je gewoon een efficiëntere manier om Dungeons en maps te maken? Vraag je jezelf af of er misschien een makkelijkere manier is? Hier is de oplossing! Met D&D mapmaker kun je zelf door middel van drag&drop functionaliteiten een speelveld maken. Dit is binnen enkele minuten al gebeurd en kan je veel tijd besparen bij je voorbereiding.</p>
+                <p>Mocht je het willen proberen, maak dan snel een gratis account aan via <a href="{{ url('/register') }}">deze link.</a></p>
             </div>
         </div>
         <div class="col-2 login-options">
